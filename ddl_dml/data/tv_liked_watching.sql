91f3a7a76f941225f6fc401be90ea8351f87cfea
Anuj Kamath <118060751+AnujKamath@users.noreply.github.com>
2024-04-17 08:21:10 +0530

    Create tv_liked_watching.sql

---
+++ ddl_dml/data/tv_liked_watching.sql
@@ -0,0 +1,50 @@
+INSERT INTO tv_liked VALUES ( 1, 1);  
+INSERT INTO tv_liked VALUES ( 1, 2);  
+INSERT INTO tv_liked VALUES ( 1, 3); 
+-- Alice Smith's liked TV sho
+INSERT INTO tv_liked VALUES ( 2, 4);  
+INSERT INTO tv_liked VALUES ( 2, 5);  
+-- Michael Johnson's liked TVhows
+INSERT INTO tv_liked VALUES ( 3, 6);  
+INSERT INTO tv_liked VALUES ( 3, 7);  
+-- Emily Brown's liked TV sho
+INSERT INTO tv_liked VALUES ( 4, 5);  
+INSERT INTO tv_liked VALUES ( 4, 8);  
+INSERT INTO tv_liked VALUES ( 4, 10);
+-- David Lee's liked TV sh
+INSERT INTO tv_liked VALUES ( 5, 1); 
+INSERT INTO tv_liked VALUES ( 5, 9);
+-- Sarah Johnson's liked TV ss
+INSERT INTO tv_liked VALUES ( 6, 1); 
+INSERT INTO tv_liked VALUES ( 6, 2); 
+INSERT INTO tv_liked VALUES ( 6, 4); 
+-- Jacob Martinez's liked TV ws
+INSERT INTO tv_liked VALUES ( 7, 3); 
+INSERT INTO tv_liked VALUES ( 7, 5); n
+-- Anuj Kamath's liked TV sh
+INSERT INTO tv_liked VALUES ( 8, 2); 
+INSERT INTO tv_liked VALUES ( 8, 9);
+-- Balaji Ramadhurai's liked shows
+INSERT INTO tv_liked VALUES ( 9, 6); 
+INSERT INTO tv_liked VALUES ( 9, 7);
+
+
+-- Inserting data into tv_watching table
+-- John Doe
+INSERT INTO tv_watching VALUES ( 1, 1, 3, 1, CURRENT_TIMESTAMP);  
+-- Alice Smi
+INSERT INTO tv_watching VALUES ( 2, 4, 1, 1, CURRENT_TIMESTAMP);  
+-- Michael Johns
+INSERT INTO tv_watching VALUES ( 3, 6, 5, 1, CURRENT_TIMESTAMP);
+-- Emily Bro
+INSERT INTO tv_watching VALUES ( 4, 5, 4, 1, CURRENT_TIMESTAMP); 
+-- David L
+INSERT INTO tv_watching VALUES ( 5, 1, 2, 2, CURRENT_TIMESTAMP);  
+-- Sarah Johns
+INSERT INTO tv_watching VALUES ( 6, 1, 1, 1, CURRENT_TIMESTAMP);  
+-- Jacob Martin
+INSERT INTO tv_watching VALUES ( 7, 3, 1, 1, CURRENT_TIMESTAMP);  
+-- Anuj Kama
+INSERT INTO tv_watching VALUES ( 8, 9, 1, 2, CURRENT_TIMESTAMP); 
+-- Balaji Ramadhur
+INSERT INTO tv_watching VALUES ( 9, 6, 2, 3, CURRENT_TIMESTAMP);
